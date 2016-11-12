59041acdb07ca6669dd443c025c80a5a6d2cdf7e
Nina Wilson <nina.wilson.mlis@gmail.com>
2016-11-11 21:14:21 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+class WelcomeController < ApplicationController
+  def index
+  	@travel_pics = ['augusta.jpeg', 'bahamas.jpeg',
+ 'ghana.jpg', 'costarica.jpeg', 'guinea.jpg', 'nigeria.jpg', 'st.kitts.jpg']
+  end
+
+  def about
+  	@color = params[:color]
+  	@shoesize = params[:size].to_i
+  	@homeland = 'Augusta, GA, United States of America'
+  	@countries = ['Bahamas','St. Kitts','Nigeria','Ghana','Guinea','Costa Rica','United States of America']
+  end
+end
+end
+end
